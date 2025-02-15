e86c2cf3f23a663b10c28696789c930f0028f98c
YOUR_NAME <rafael_minetti@hotmail.com>
2025-02-14 22:07:29 -0300

    feat: configurate router and pages

---
+++ src/router.jsx
@@ -0,0 +1,46 @@
+import { createBrowserRouter } from "react-router-dom";
+import RootLayout from "./pages/RootLayout";
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+import ProductEdit from "./pages/ProductEdit";
+import ProductCreate from "./pages/ProductCreate";
+import productLoader from "./loaders/productLoader";
+import ProductBoundary from "./error-boundaries/ProductBoundary";
+import Products from "./pages/Products";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <RootLayout />,
+		children: [
+			[
+				{
+					index: true,
+					element: <Home />,
+				},
+				{
+					path: "products",
+					element: <Products />,
+				},
+				{
+					path: "products/:productId",
+					element: <ProductDetails />,
+					loader: productLoader,
+					errorElement: <ProductBoundary />,
+				},
+				{
+					path: "product-edit/:productId",
+					element: <ProductEdit />,
+					loader: productLoader,
+					errorElement: <ProductBoundary />,
+				},
+				{
+					path: "product-create",
+					element: <ProductCreate />,
+				},
+			],
+		],
+	},
+]);
+
+export default router;
