3abfa5251d26f6b97a4e3cc1adee2ca2395b849a
Rafael Minetti <rafael_minetti@hotmail.com>
2025-05-25 17:06:32 -0300

    feat: Implement home screen with product listing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,37 @@
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import { useState } from "react";
+
 export default function Home() {
-	return <h2>Ayoyoyo</h2>;
+	// Get the products from the Products component
+	const [products] = useState([
+		{ id: 1, name: "Product 1", price: 10, quantity: 5 },
+		{ id: 2, name: "Product 2", price: 20, quantity: 10 },
+	]);
+
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="col-md-12">
+					{products.length > 0 ? (
+						<>
+							<h2>Most Recent Products</h2>
+							<ul>
+								{products.slice(0, 5).map((product) => (
+									<li key={product.id}>{product.name}</li>
+								))}
+							</ul>
+						</>
+					) : (
+						<>
+							<p>No products registered yet.</p>
+							<Link to="/product-create">
+								<Button variant="primary">New Product</Button>
+							</Link>
+						</>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 }
